d4589b7d5406669d3153e1c2c7f6b398ea39f0b2
shamanaz12 <160182090+shamanaz12@users.noreply.github.com>
2024-02-26 22:39:37 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,15 @@
+let personName: string = "";
+personName = prompt(`What is your name?`) || "";
+let lowercase: string = personName.toLowerCase();
+let uppercase: string = personName.toUpperCase();
+let titlecase: string = personName
+  .split(``)
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+  .join(``);
+
+if (personName !== null && personName !== ``) {
+  alert(`Hello ${personName}, here are your name in:
+    Lowercase: ${lowercase}
+    Uppercase: ${uppercase}
+    Titlecase: ${titlecase}`);
+}
